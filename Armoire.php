77d19ba9dc856afe69955413865944791d1550c2
marineco <marinecolonge@gmail.com>
2017-02-02 10:19:52 +0100

    création d'un instance d'idiorm dans le constructeur

---
+++ Armoire.php
@@ -0,0 +1,21 @@
+<?php 
+	
+require 'vendor/autoload.php';
+
+
+class Armoire {
+	
+	private $db;
+
+	public function __construct() {
+
+		ORM::configure('mysql:host=localhost;dbname=mon_armoire');
+		ORM::configure('username', 'root');
+		ORM::configure('password', 'root');
+
+		$this->db = ORM::for_table('mes_chaussettes');
+	}
+
+}
+
+?>
