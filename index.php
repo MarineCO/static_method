52c4f0b90a2f7b0b8fe6ea9c2f8b6648909e81a1
marineco <marinecolonge@gmail.com>
2017-02-02 10:46:04 +0100

    création index.php pour affichage des 50 premières entrées de la table

--- index.php
+++ index.php
@@ -1,12 +1,40 @@
 <?php 
-	
-require 'vendor/autoload.php';
 
-class Armoire {
-	
-	function __construct(argument) {
-		
-	}
-}
+require 'Armoire.php';
 
-?>
+$armoire = Armoire::all(50, 0);
+
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercice Static Method</title>
+</head>
+<body>
+
+	<table>
+		<tr>
+			<td>id</td>
+			<td>pointure</td>
+			<td>temp_lavage</td>
+			<td>description</td>
+			<td>couleur</td>
+			<td>date_lavage</td>
+		</tr>
+
+		<?php foreach ($armoire as $chaussette): ?>
+			<tr>
+				<td><?= $chaussette->id; ?></td>
+				<td><?= $chaussette->pointure; ?></td>
+				<td><?= $chaussette->temp_lavage ?></td>
+				<td><?= $chaussette->description; ?></td>
+				<td><?= $chaussette->couleur ?></td>
+				<td><?= $chaussette->date_lavage; ?></td>
+			</tr>
+
+		<?php endforeach ?>
+	</table>
+	
+</body>
+</html>
