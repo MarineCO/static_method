4110f69d08b4380998263fb3b4019174ff91bc80
marineco <marinecolonge@gmail.com>
2017-02-02 16:01:58 +0100

    via l'id d'une entrée, affichage d'une autre page récapitulant les détails de cette entrée

--- index.php
+++ index.php
@@ -5,31 +5,45 @@ require 'Armoire.php';
 $armoire = Armoire::all(50, 0);
 
 
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Exercice Static Method</title>
-</head>
-<body>
+if (isset($_GET['id'])) {
 
-	<table>
-		<tr>
-			<td>id</td>
-			<td>pointure</td>
-			<td>temp_lavage</td>
-			<td>description</td>
-			<td>couleur</td>
-			<td>date_lavage</td>
-		</tr>
+	$getDataId = Armoire::get($_GET['id']);
 
-		<?php foreach ($armoire as $chaussette): ?>
+	echo '<div>Id : ' .$getDataId->id. '</div>';
+	echo '<div>Couleur : ' .$getDataId->couleur. '</div>';
+	echo '<div>Pointure : ' .$getDataId->pointure. '</div>';
+	echo '<div>Température de lavage : ' .$getDataId->temp_lavage. '</div>';
+	echo '<div>Description : ' .$getDataId->description. '</div>';
+	echo '<div>Date de lavage : ' .$getDataId->date_lavage. '</div>';
+
+
+} else {
+
+	?><!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Exercice Static Method</title>
+	</head>
+	<body>
+
+		<table>
+			<tr>
+				<td>id</td>
+				<td>pointure</td>
+				<td>temp_lavage</td>
+				<td>description</td>
+				<td>couleur</td>
+				<td>date_lavage</td>
+			</tr>
+
+			<?php foreach ($armoire as $chaussette): ?>
 			<tr>
 				<td><?= $chaussette->id; ?></td>
-				<td><?= $chaussette->pointure; ?></td>
-				<td><?= $chaussette->temp_lavage ?></td>
-				<td><?= $chaussette->description; ?></td>
 				<td><?= $chaussette->couleur ?></td>
+				<td><?= $chaussette->pointure; ?></td>
+				<td><?= $chaussette->temp_lavage ?> °C</td>
+				<td><?= $chaussette->description; ?></td>
 				<td><?= $chaussette->date_lavage; ?></td>
 			</tr>
 
@@ -38,3 +52,4 @@ $armoire = Armoire::all(50, 0);
 	
 </body>
 </html>
+<?php } ?>
